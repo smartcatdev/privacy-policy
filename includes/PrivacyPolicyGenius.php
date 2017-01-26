36a82d10d8f7ffead9c99296166ccc1cc783db8b
smartcat-ericgreen <eric@smartcat.ca>
2017-01-26 11:25:22 -0500

    Add remaining php files and add temp shortcode

--- includes/PrivacyPolicyGenius.php
+++ includes/PrivacyPolicyGenius.php
@@ -8,5 +8,17 @@ class PrivacyPolicyGenius extends AbstractPlugin {
 
     public function start() {
         $this->add_api_subscriber( include_once $this->dir . '/config/admin_settings.php' );
+
+        add_shortcode( 'privacy_policy2', function () {
+            include_once $this->dir . '/templates/template.php';
+        } );
+    }
+
+    public function activate() {
+        $option = get_option( 'policy-plugin-options' );
+
+        if( $option != false ) {
+            include_once $this->dir . '/migrate.php';
+        }
     }
 }
---
+++ templates/template.php
@@ -0,0 +1,4 @@
+<?php
+
+
+var_dump( unserialize( get_option( 'policy-plugin-options' ) ) );
