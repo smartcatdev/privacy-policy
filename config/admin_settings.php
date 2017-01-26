dc476c0b604b9721758f65a54489a4dd48f11291
smartcat-ericgreen <eric@smartcat.ca>
2017-01-26 12:01:04 -0500

    Add some settings

--- config/admin_settings.php
+++ config/admin_settings.php
@@ -2,7 +2,11 @@
 
 namespace privacy_policy_genius;
 
+use privacy_policy_genius\admin\RadioGroupField;
+use smartcat\admin\CheckBoxField;
+use smartcat\admin\MatchFilter;
 use smartcat\admin\SettingsPage;
+use smartcat\admin\SettingsSection;
 
 $admin = new SettingsPage(
     array(
@@ -11,5 +15,33 @@ $admin = new SettingsPage(
         'menu_slug' => 'privacy_guru'
     )
 );
+
+$section_3 = new SettingsSection( 'section_3', '' );
+
+$section_3->add_field( new CheckBoxField(
+    array(
+        'id'            => 'privacy_policy_third_party_info',
+        'option'        => '',
+        'value'         => '',
+        'label'         => __( 'Personal information transfer', PLUGIN_ID ),
+        'desc'          => __( 'Does your website transfer personal information to third parties', PLUGIN_ID ),
+        'validators'    => array( new MatchFilter( array( '', 'on' ), '' ) )
+    )
+
+) )->add_field( new RadioGroupField(
+    array(
+        'id' => 'privacy_policy_destroy_information',
+        'option'        => '',
+        'value'         => '',
+        'break'         => true,
+        'label'         => __( 'Personal information disposal', PLUGIN_ID ),
+        'desc'          => __( 'How do you dispose personal information?', PLUGIN_ID ),
+        'options'       => $options = array( 'destroy' => __( 'Destroy', PLUGIN_ID ), 'erase' => __( 'Erase', PLUGIN_ID ) ),
+        'validators'    => array( new MatchFilter( $options, '' ) )
+    )
+
+) );
+
+$admin->add_section( $section_3 );
 
 return $admin;
---
+++ includes/admin/RadioGroupField.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace privacy_policy_genius\admin;
+
+
+use smartcat\admin\SettingsField;
+
+class RadioGroupField extends SettingsField {
+    protected $options;
+    protected $break = false;
+
+    public function __construct( array $args ) {
+        parent::__construct( $args );
+
+        $this->break = $args['break'];
+        $this->options = $args['options'];
+    }
+
+    public function render( array $args ) { ?>
+
+        <fieldset>
+
+            <?php foreach( $this->options as $option => $title ) : ?>
+
+                <label>
+                    <input type="radio"
+                           name="<?php esc_attr_e( $this->option ); ?>"
+                           value="<?php esc_attr_e( $option ); ?>"
+                           <?php checked( $this->value, 'on' ); ?> />
+
+                    <?php esc_html_e( $title ); ?>
+
+                </label>
+
+                <?php if( $this->break ) : ?><br><?php endif; ?>
+
+            <?php endforeach; ?>
+
+        </fieldset>
+
+    <?php }
+}
