2da78074a77f4b2988145c65aa4ffbad1de4b2bd
smartcat-ericgreen <eric@smartcat.ca>
2017-01-27 14:20:34 -0500

    Fix js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,7 +3,7 @@
 jQuery( "document" ).ready( function ( $ ) {
 
     var checkbox = $( "#privacy_policy_info_transfer" );
-    var section = $( 'input[name="privacy_policy_transfer_purpose[]"]' ).parents( 'tr' );
+    var section = $( 'input[name="privacy_policy_genius_personal-information-transfer-purpose[]"]' ).parents( 'tr' );
 
     if( !checkbox.attr( 'checked' ) ) {
         section.hide();
